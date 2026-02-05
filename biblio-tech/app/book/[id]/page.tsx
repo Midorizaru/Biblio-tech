724783e5bc56534a1dccd3d242b9079b2f1e5f04
Midorizaru <thomas.schaller@supinfo.com>
2026-02-05 16:33:13 +0100

    différentes pages mises en place (priorité ux)

---
+++ biblio-tech/app/book/[id]/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export default function BookPage({ params }: any) {
+  const [book, setBook] = useState<any>(null);
+
+  useEffect(() => {
+    fetch("https://openlibrary.org" + params.id + ".json")
+      .then((response) => response.json())
+      .then((data) => {
+        setBook(data);
+      });
+  }, [params.id]);
+
+  if (!book) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div>
+      <h1>{book.title}</h1>
+
+      {book.description && (
+        <p>
+          {typeof book.description === "string"
+            ? book.description
+            : book.description.value}
+        </p>
+      )}
+    </div>
+  );
+}
---
+++ biblio-tech/app/search/page.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import Link from "next/link";
+
+export default function QuickSearch() {
+  const searchParams = useSearchParams();
+  const query = searchParams.get("q");
+
+  const [books, setBooks] = useState<any[]>([]);
+
+  useEffect(() => {
+    if (!query) {
+      return;
+    }
+
+    fetch("https://openlibrary.org/search.json?q=" + query)
+      .then((response) => response.json())
+      .then((data) => {
+        setBooks(data.docs);
+      });
+  }, [query]);
+
+  return (
+    <div>
+      <h1>Results for "{query}"</h1>
+
+      {books.map((book) => (
+        <div key={book.key}>
+          <p>{book.title}</p>
+
+          <Link href={"/book" + book.key}>
+            See details
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+}
